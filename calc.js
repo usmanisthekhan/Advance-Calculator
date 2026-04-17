ccb2f968a0e1932a90f175362196615748a7d71b
Usman Khan <usmanisthekhan@gmail.com>
2026-04-17 17:49:33 +0500

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,24 @@
+const add = (a, b) => {
+  return a + b;
+};
+const mul = (a, b) => {
+  return a * b;
+};
+const div = (a, b) => {
+  return a / b;
+};
+const sub = (a, b) => {
+  return a - b;
+};
+
+// for addition
+console.log(add(10, 2));
+
+// for multiplication
+console.log(mul(10, 2));
+
+// for division
+console.log(div(10, 2));
+
+// for subtraction
+console.log(sub(10, 2));
